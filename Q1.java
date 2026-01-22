e3f8ce63c3a03c3d8bdba04ecb46ffa1087b6c6b
Koustuv Samanta <koustuvsamanta2006@gmail.com>
2026-01-22 20:21:08 +0530

    Remove Student class and related functionality

--- Q1.java
+++ Q1.java
@@ -1,38 +1,4 @@
-import java.util.Scanner;
 
-abstract class Student {
-    long rollno;
-    long regno;
-
-    void getinput() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter roll no: ");
-        rollno = sc.nextInt();
-        System.out.println("Enter registration number : ");
-        regno = sc.nextLong();
-    }
-    abstract void course();
-}
-class Kiitian extends Student {
-
-    void course(){
-        System.out.println("Course-B.Tech(Computer Science & Engg");
-    }
-    void display(){
-        System.out.println("Rollno:"+ rollno);
-        System.out.println("Registrationno:"+ regno);
-        course();
-    }
-}
-
-public class Q1{
-    public static void main(String[] args){
-        Kiitian k = new Kiitian();
-        k.getinput();
-        k.display();
-    }
-
-}
 
 
 
